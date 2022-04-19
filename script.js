b4b2a4ce6744487a16e3f75085c0429e5102243e
Aniel Rocha <81044475+anielt@users.noreply.github.com>
2022-04-19 10:06:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,10 @@ function random_image() {
   console.log(array_of_images);
   array_of_images.push(imageA);
   array_of_images.push(imageB);
-  array_of_images.push(imageC);
+  array_of_images.push(imageD);
+  array_of_images.push(imageE);
+  array_of_images.push(imageF);
+  array_of_images.push(imageG);
   var choice = Math.floor(Math.random() * array_of_images.length);
   document.getElementById("container").innerHTML = "<img src='" 
       + array_of_images[choice] + "'></img>";
