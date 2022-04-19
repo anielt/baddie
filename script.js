2640c0c3222d0663eaaecaf1eb84d3939fb882ad
Aniel Rocha <81044475+anielt@users.noreply.github.com>
2022-04-19 10:04:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function random_image() {
-  var imageA = "https://i.imgur.com/6H9D8To.jpg";
-  var imageB = "https://i.imgur.com/5CeeOwo.jpg";
-  var imageC = "https://i.imgur.com/5CQriHS.jpg";
+  var imageA = "https://i.imgur.com/6Op142i.png";
+  var imageB = "https://i.imgur.com/nMbfMcA.png";
+  var imageC = "https://i.imgur.com/cBGhzfK.png";
+  var imageD = "https://i.imgur.com/BIDDCeJ.png";
+  var imageE = "https://i.imgur.com/XQfBPcf.png";
+  var imageG = "https://i.imgur.com/wcZJxmU.png";
+  var imageF = "https://i.imgur.com/T3Ny5Ro.png";
+
   var array_of_images = new Array ();
   console.log(array_of_images);
   array_of_images.push(imageA);
