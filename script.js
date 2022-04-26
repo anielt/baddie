8b2f25115be286acbaaf4d480f5a23f7049350e7
Aniel Rocha <81044475+anielt@users.noreply.github.com>
2022-04-26 10:29:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function random_image() {
-  var imageA = "https://i.imgur.com/6Op142i.png";
+  var imageA = "https://i.imgur.com/kWiO17S.png";
   var imageB = "https://i.imgur.com/nMbfMcA.png";
   var imageC = "https://i.imgur.com/cBGhzfK.png";
   var imageD = "https://i.imgur.com/BIDDCeJ.png";
   var imageE = "https://i.imgur.com/XQfBPcf.png";
-  var imageG = "https://i.imgur.com/wcZJxmU.png";
-  var imageF = "https://i.imgur.com/T3Ny5Ro.png";
+  var imageF = "https://i.imgur.com/wcZJxmU.png";
+  var imageG = "https://i.imgur.com/T3Ny5Ro.png";
+  var imageH = "https://i.imgur.com/QyoyWdW.png";
 
   var array_of_images = new Array ();
   console.log(array_of_images);
@@ -15,6 +16,7 @@ function random_image() {
   array_of_images.push(imageE);
   array_of_images.push(imageF);
   array_of_images.push(imageG);
+  array_of_images.push(imageH);
   var choice = Math.floor(Math.random() * array_of_images.length);
   document.getElementById("container").innerHTML = "<img src='" 
       + array_of_images[choice] + "'></img>";
